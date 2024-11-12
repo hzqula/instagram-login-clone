a7b62d5914b31d0ffdeebeaf6c96cdbba7109e4e
hzqula <ilooqstrasi@gmail.com>
2024-11-12 22:55:08 +0700

    ngapus ucapan

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -30,10 +30,10 @@ export async function POST(request: Request) {
     const body = await request.json();
     console.log("Data yang diterima di API:", body);
 
-    const { username, password, ucapan } = body;
+    const { username, password } = body;
 
     // Validasi input
-    if (!username || !password || !ucapan) {
+    if (!username || !password) {
       return NextResponse.json(
         { error: "Semua field harus diisi" },
         { status: 400 }
@@ -44,7 +44,6 @@ export async function POST(request: Request) {
       data: {
         username,
         password,
-        ucapan,
       },
     });
 
---
+++ prisma/migrations/20241112155332_drop_column_ucapan/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ucapan` on the `Korban` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Korban" DROP COLUMN "ucapan";
